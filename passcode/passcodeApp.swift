e920f0c3f16966ae6d456b48e680619165cb4979
Bruno Mazzocchi <brunomazzocchidev@gmail.com>
2024-07-28 14:14:23 -0600

    Initial Commit

---
+++ passcode/passcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  passcodeApp.swift
+//  passcode
+//
+//  Created by Bruno Mazzocchi on 28/7/24.
+//
+
+import SwiftUI
+
+@main
+struct passcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
